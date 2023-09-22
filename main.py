0c33cc337bc832f0f5d5b9e1e4e730fc7d6c475f
MC <mc8@protonmail.com>
2023-09-22 13:08:16 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Blocks websites
+
+import os
+
+def block_it(url):
+    print("Working on it")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    website = "gop.com"
+    block_it(website)
+
