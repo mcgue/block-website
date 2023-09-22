eec95f76dc1fcb48d88fc0d832b367823405bf34
MC <mc8@protonmail.com>
2023-09-22 13:12:46 -0700

    code added but need to get admin permission

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ import os
 
 def block_it(url):
     print("Working on it")
+    # Modify the system's host file to block the website
+    try:
+        # Add the website URL to the host file, redirecting to localhost
+        with open('/etc/hosts' if os.name != 'nt' else r'C:\Windows\System32\drivers\etc\hosts', 'a') as hosts_file:
+            hosts_file.write('127.0.0.1\t{}\n'.format(url))
+        print('Website blocked successfully.')
+    except PermissionError:
+        print('Permission denied. Run the script as administrator.')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
